3b68e687d163c8234fef67e1f8b423b172953334
andrei-varchanka <53002740+andrei-varchanka@users.noreply.github.com>
2023-05-03 20:03:20 +0200

    projects grid

---
+++ src/app/components/projects/projects.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent implements OnInit {
+
+  projects = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
